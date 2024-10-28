c5c1ab14c5e4906d8230f32cfea4f68e327fbc12
Vinícius Marques <marquesv473@gmail.com>
2024-10-28 08:56:07 -0400

    Fix: SVG sumindo em navegadores WebKit

--- src/lib/Balao.svelte
+++ src/lib/Balao.svelte
@@ -2,7 +2,7 @@
     let props = $props();
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" width="auto" height="auto"
+<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"
     viewBox="0 0 264.525 471.517">
     <g id="Balão">
         <path
